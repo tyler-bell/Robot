963090a4efe1449b182b612c047eb1b1d57a9b72
Tyler Bell <100159509@student.georgianc.on.ca>
2018-07-23 20:52:17 -0400

    added shoddy counter

---
+++ MyLittleRobot/Robot2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLittleRobot {
+    class Robot2 : Robot {
+
+        private int counter;
+
+        public Robot2(Point start) : base(start) {
+
+        }
+
+        public int Counter { get => counter; set => counter = value; }
+        
+        public override void Move(int spaces) {
+            Point temp = Coords;
+            base.Move(spaces);
+            if (temp != Coords) {
+                Counter += spaces;
+            }
+        }
+    }
+    
+}
